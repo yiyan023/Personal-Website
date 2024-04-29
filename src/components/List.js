500004b7743c6fe487ee6594fb4c2ba0bdb6b35d
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-04-29 00:53:45 -0400

    update for today

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import { Container, Row, Col } from "react-bootstrap";
 
 import Python from '../assets/python.png'
@@ -12,26 +12,35 @@ import Git from '../assets/git.png'
 import Bootstrap from '../assets/bootstrap.png'
 import Flutter from '../assets/flutter.png'
 
+import AOS from 'aos';
+import 'aos/dist/aos'
+
 export const List = () => {
-  return (
-	<div className='list'>
-		<h1 className="name"> Top Skills: </h1>
-		<Container>
-			<Row className='align-items-center'>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={Python}/><p>Python</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={JavaScript}/><p>JavaScript</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={C}/><p>C</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={ReactJS}/><p>React</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={Flask}/><p>Flask</p></Col>
-			</Row>
-			<Row className='align-items-center'>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={Tailwind}/><p>Tailwind</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={MongoDB}/><p>MongoDB</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={Git}/><p>Git</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={Bootstrap}/><p>Bootstrap</p></Col>
-				<Col className='align-items-center justify-content-center d-flex portrait border border-2 border-white flex-column pt-5 pb-5 m-2'><img src={Flutter}/><p>Flutter</p></Col>
-			</Row>
-		</Container>
-	</div>
-  )
+	useEffect(() => {
+		AOS.init({
+			duration: 1000
+		});
+	}, []);
+
+  	return (
+		<div className='list'>
+			<h1 data-aos="fade-in" data-aos-delay="500" className="name"> Top Skills </h1>
+			<Container>
+				<Row className='align-items-center justify-content-center'>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1000" className='skill'><img src={Python}/><p>Python</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1000" className='skill'><img src={JavaScript}/><p>JavaScript</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1000" className='skill'><img src={C}/><p>C</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1000" className='skill'><img src={ReactJS}/><p>React</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1000" className='skill'><img src={Flask}/><p>Flask</p></Col>
+				</Row>
+				<Row className='align-items-center justify-content-center'>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1250" className='skill'><img src={Tailwind}/><p>Tailwind</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1250" className='skill'><img src={MongoDB}/><p>MongoDB</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1250" className='skill'><img src={Git}/><p>Git</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1250" className='skill'><img src={Bootstrap}/><p>Bootstrap</p></Col>
+					<Col xl={2} xs={10} data-aos="fade-in" data-aos-delay="1250" className='skill'><img src={Flutter}/><p>Flutter</p></Col>
+				</Row>
+			</Container>
+		</div>
+  	)
 }
